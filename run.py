08fa43251b3156aa4a0012909dc7a6eb47feb261
chandu16 <jagadishdevu523@icloud.com>
2025-03-17 20:35:31 +0530

    ML Pipeline training

--- run.py
+++ run.py
@@ -1,5 +1,15 @@
 from ml_package.package.feature.data_processing import get_feature_dataframe
 
+from ml_package.package.ml_training.retrival import get_train_test_score_set
+from ml_package.package.ml_training.train import train_model
+from ml_package.package.ml_training.preprocessing_pipeline import get_pipeline
+
+from ml_package.package.utils.utils import set_or_create_experiment
+from ml_package.package.utils.utils import get_performance_plots
+from ml_package.package.utils.utils import get_classification_metrics
+from ml_package.package.utils.utils import register_model_with_client
+import mlflow
+
 if __name__ == "__main__":
     experiment_name = "house_pricing_classifier"
     run_name = "training_classifier"
@@ -7,4 +17,22 @@ if __name__ == "__main__":
     artifact_path = "model"
 
     df = get_feature_dataframe()
-    print(df.head())
+
+    x_train, x_test, x_score, y_train, y_test, y_score = get_train_test_score_set(df)
+
+    features = [f for f in x_train.columns if f not in ["id", "target", "MedHouseVal"]]
+
+    pipeline = get_pipeline(numerical_features=features, categorical_features=[])
+
+    experiment_id = set_or_create_experiment(experiment_name=experiment_name)
+
+    run_id, model = train_model(pipeline=pipeline, run_name=run_name, model_name=model_name, artifact_path=artifact_path, x=x_train[features], y=y_train)
+
+    y_pred = model.predict(x_test)
+
+    performance_plots = get_performance_plots(y_true=y_test,y_pred=y_pred,prefix='test')
+
+    # log performance metrics
+    with mlflow.start_run(run_id=run_id):
+        for plot_name, fig in performance_plots.items():
+            mlflow.log_figure(fig, plot_name + ".png")
